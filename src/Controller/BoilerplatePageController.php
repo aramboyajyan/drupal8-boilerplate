f05e9750eff1461841f9015afbf54ec623fbbcdf
Aram Boyajyan <dev@aram.cz>
2016-07-13 09:00:34 +0200

    adding the sample page with arguments

--- src/Controller/BoilerplatePageController.php
+++ src/Controller/BoilerplatePageController.php
@@ -10,11 +10,23 @@ use Drupal\Core\Controller\ControllerBase;
 class BoilerplatePageController extends ControllerBase {
 
   /**
-   * Returns build array for our sample page.
+   * Returns build array for the simple page.
    */
-  public function showPage() {
+  public function showSimplePage() {
     return [
-      '#markup' => $this->t('This is a sample page.'),
+      '#markup' => $this->t('This is a simple page.'),
+    ];
+  }
+
+  /**
+   * Returns build array for the arguments page.
+   */
+  public function showArgumentsPage($arg1, $arg2) {
+    return [
+      '#markup' => $this->t('This is the page demonstrating how to pass arguments through URL. Firsrt argument is: %arg1 and the second one is %arg2.', [
+        '%arg1' => $arg1,
+        '%arg2' => $arg2,
+      ]),
     ];
   }
 
